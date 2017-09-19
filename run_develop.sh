2c8700041fc04cc6d0d790cb81c9430b1b2d0f95
Bibek Dahal <bibekdahal.bd16@gmail.com>
2017-09-19 23:18:21 +0545

    Setup channels and test websocket

--- run_develop.sh
+++ run_develop.sh
@@ -4,7 +4,8 @@
 cd backend
 python manage.py migrate --no-input
 python manage.py createinitialrevisions
-python manage.py runserver 0.0.0.0:8000 &
+python manage.py runserver 0.0.0.0:8000 --noworker &
+python manage.py runworker &
 celery -A deep worker -l info &
 
 cd ../frontend
