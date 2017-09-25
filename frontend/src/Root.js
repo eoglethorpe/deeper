734a7678f695c4ace32d379f7118815d7ada8d53
tnagorra <weathermist@gmail.com>
2017-09-25 11:04:37 +0545

    Show empty div unless store is rehydrated

--- frontend/src/Root.js
+++ frontend/src/Root.js
@@ -6,15 +6,38 @@ import { persistStore } from 'redux-persist';
 import configureStore from './common/utils/configureStore';
 import App from './App';
 
+// FIXME: configure this somewhere else so that it can be referenced
+// from elsewhere
 const store = configureStore();
-persistStore(store);
 
-export default function Root() {
-    return (
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
-    );
+export default class Root extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { rehydrated: false };
+        this.store = store;
+    }
+
+    componentWillMount() {
+        const afterRehydrateCallback = () => this.setState({ rehydrated: true });
+        // FIXME: load params and pass it instead of undefined
+        persistStore(this.store, undefined, () => afterRehydrateCallback);
+    }
+
+    render() {
+        if (!this.state.rehydrated) {
+            // Empty div unless the redux store is not rehydrated
+            return (
+                <div />
+            );
+        }
+
+        return (
+            <Provider store={this.store}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </Provider>
+        );
+    }
 }
