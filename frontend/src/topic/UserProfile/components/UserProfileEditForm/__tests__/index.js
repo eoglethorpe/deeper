4c9ffbe1e560544a7a86fb16970d621b820afd22
pprabesh <prabes.pathak@gmail.com>
2017-10-27 16:11:32 +0545

    Fix tests for UserProfileEditForm

--- frontend/src/topic/UserProfile/components/UserProfileEditForm/__tests__/index.js
+++ frontend/src/topic/UserProfile/components/UserProfileEditForm/__tests__/index.js
@@ -3,10 +3,23 @@ import { shallow } from 'enzyme';
 import UserProfileEditForm from '../';
 
 describe('<UserProfileEditForm />', () => {
+    const formValues = {
+        firstName: 'John',
+        lastName: 'Doe',
+        organization: 'Toggle',
+    };
+
+    const handleFormCancel = (e) => {
+        e.preventDefault();
+        this.props.onCancel();
+    };
+
     const wrapper = shallow(
         <UserProfileEditForm
             pending={false}
             onSubmit={() => {}}
+            onCancel={handleFormCancel}
+            formValues={formValues}
         />,
     );
 
