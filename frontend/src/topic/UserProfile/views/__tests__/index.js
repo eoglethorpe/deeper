a2ddb986fa3bfe69d20c2005febe4905cb28629f
pprabesh <prabes.pathak@gmail.com>
2017-11-01 12:09:23 +0545

    Test for User Profile Edit Form render

--- frontend/src/topic/UserProfile/views/__tests__/index.js
+++ frontend/src/topic/UserProfile/views/__tests__/index.js
@@ -3,6 +3,8 @@ import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import { shallow } from 'enzyme';
 import UserProfile from '../';
+import Form, { requiredCondition } from '../../../../public/components/Form';
+import TextInput from '../../../../public/components/TextInput';
 
 const initialState = {
 };
@@ -16,6 +18,45 @@ describe('<UserProfile />', () => {
             store={store}
         >
             <UserProfile />
+        </Provider>,
+    );
+
+    it('renders properly', () => {
+        expect(wrapper.length).toEqual(1);
+    });
+});
+
+describe('<User Profile Edit Form />', () => {
+    const mockStore = configureStore();
+    const store = mockStore(initialState);
+
+    const elements = [
+        'firstName',
+        'lastName',
+        'organization',
+    ];
+
+    const validation = {};
+    const validations = {
+        firstName: [requiredCondition],
+        lastName: [requiredCondition],
+        organization: [requiredCondition],
+    };
+
+    const wrapper = shallow(
+        <Provider
+            store={store}
+        >
+            <Form
+                changeCallback={() => {}}
+                elements={elements}
+                failureCallback={() => {}}
+                successCallback={() => {}}
+                validation={validation}
+                validations={validations}
+            >
+                <TextInput />
+            </Form>
         </Provider>,
     );
 
