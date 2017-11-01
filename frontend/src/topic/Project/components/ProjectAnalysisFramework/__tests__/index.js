9eb07bbfbaf0489c00e74d2b12259a694828b64b
pprabesh <prabes.pathak@gmail.com>
2017-11-01 13:38:32 +0545

    Add basic render test for Project Panel

---
+++ frontend/src/topic/Project/components/ProjectAnalysisFramework/__tests__/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { shallow } from 'enzyme';
+import ProjectAnalysisFramework from '../index';
+
+const initialState = {
+};
+
+describe('<ProjectAnalysisFramework />', () => {
+    const mockStore = configureStore();
+    const store = mockStore(initialState);
+
+    const wrapper = shallow(
+        <Provider
+            store={store}
+        >
+            <ProjectAnalysisFramework />
+        </Provider>,
+    );
+
+    it('renders properly', () => {
+        expect(wrapper.length).toEqual(1);
+    });
+});
