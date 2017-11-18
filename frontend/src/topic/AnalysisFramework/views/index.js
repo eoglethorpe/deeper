28033e8597a8c9a9804f29f7aa972bad7e0c2e27
Frozen Helium <fren.ankit@gmail.com>
2017-11-18 22:29:45 +0545

    Add basic grid layout

--- frontend/src/topic/AnalysisFramework/views/index.js
+++ frontend/src/topic/AnalysisFramework/views/index.js
@@ -17,11 +17,27 @@ import {
     activeProjectSelector,
 } from '../../../common/selectors/siloDomainData';
 
+import {
+    Responsive,
+} from '../../../public/components/General';
+
+import {
+    TextInput,
+} from '../../../public/components/Input';
+
+import {
+    TransparentButton,
+} from '../../../public/components/Action';
+
 import styles from './styles.scss';
 
 const propTypes = {
     setNavbarState: PropTypes.func.isRequired,
     activeProject: PropTypes.number.isRequired, // eslint-disable-line
+    boundingClientRect: PropTypes.shape({
+        width: PropTypes.number,
+        height: PropTypes.number,
+    }).isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -33,10 +49,24 @@ const mapDispatchToProps = dispatch => ({
     setNavbarState: params => dispatch(setNavbarStateAction(params)),
 });
 
+@Responsive
 @connect(mapStateToProps, mapDispatchToProps)
 @CSSModules(styles, { allowMultiple: true })
 export default class AnalysisFramework extends React.PureComponent {
     static propTypes = propTypes;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [
+                {
+                    key: 'a',
+                    gridData: { x: 2, y: 2, w: 30, h: 20 },
+                },
+            ],
+        };
+    }
 
     componentWillMount() {
         this.props.setNavbarState({
@@ -57,45 +87,88 @@ export default class AnalysisFramework extends React.PureComponent {
         });
     }
 
+    getGridItems = () => {
+        const {
+            items,
+        } = this.state;
+
+        return items.map(item => (
+            <div
+                key={item.key}
+                data-grid={item.gridData}
+                styleName="grid-item"
+            >
+                <header
+                    styleName="header"
+                >
+                    <h2>Grid item</h2>
+                    <div styleName="actions">
+                        <span
+                            styleName="drag-handle"
+                            className="ion-arrow-move drag-handle"
+                        />
+                        <TransparentButton
+                            styleName="close-button"
+                        >
+                            <span className="ion-android-close" />
+                        </TransparentButton>
+                    </div>
+                </header>
+                <div styleName="content">
+                    <TextInput
+                        label="Wow"
+                    />
+                </div>
+            </div>
+        ));
+    }
+
+    handleItemResize = (layout, oldItem, newItem) => {
+        console.log(layout, oldItem, newItem);
+    }
+
+    handleItemDragStop = (e, node) => {
+        console.log(e, node);
+    }
+
     render() {
-        const layout = [
-            { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true },
-            { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-            { i: 'c', x: 4, y: 0, w: 1, h: 2 },
-        ];
+        const {
+            width,
+            height,
+        } = this.props.boundingClientRect;
+
+        const numOfRows = 100;
+        const numOfColumns = 100;
+        const margin = [0, 0];
+        const rowHeight = parseInt((height || 0) / numOfRows, 10);
 
         return (
             <div styleName="analysis-framework">
                 <Helmet>
                     <title>{ pageTitles.analysisFramework }</title>
                 </Helmet>
-                <ReactGridLayout
-                    styleName="grid-layout"
-                    layout={layout}
-                    cols={12}
-                    rows={20}
-                    rowHeight={100}
-                    width={1200}
+                <div
+                    styleName="left"
                 >
-                    <div
-                        key="a"
-                        styleName="a"
+                    Widgets
+                </div>
+                <div
+                    styleName="right"
+                >
+                    <ReactGridLayout
+                        styleName="grid-layout"
+                        cols={numOfColumns}
+                        margin={margin}
+                        width={width || 0}
+                        rowHeight={rowHeight}
+                        compactType={null}
+                        onDragStop={this.handleItemDragStop}
+                        onResizeStop={this.handleItemResize}
+                        draggableHandle=".drag-handle"
                     >
-                        A
-                    </div>
-                    <div
-                        key="b"
-                        styleName="b"
-                    >
-                        B
-                    </div>
-                    <div
-                        key="c"
-                        styleName="c"
-                    >
-                        C
-                    </div>
-                </ReactGridLayout>
+                        { this.getGridItems() }
+                    </ReactGridLayout>
+                </div>
             </div>
         );
     }
