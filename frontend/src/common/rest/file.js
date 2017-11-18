de16d2392e879ce3070501fa5b4314eefb6cae35
thenav56 <ayernavin@gmail.com>
2017-11-18 15:30:21 +0545

    Dropbox, Google drive backend complete
    Add api point to upload google drive and dropbox files to deep
    Add basic rest in frontend

--- frontend/src/common/rest/file.js
+++ frontend/src/common/rest/file.js
@@ -1,7 +1,31 @@
 import { wsEndpoint } from '../config/rest';
 
 export const urlForUpload = `${wsEndpoint}/files/`;
+export const urlForGoogleDriveFileUpload = `${wsEndpoint}/files-google-drive/`;
+export const urlForDropboxFileUpload = `${wsEndpoint}/files-dropbox/`;
+
 export const createHeaderForFileUpload = ({ access }) => ({
     Authorization: `Bearer ${access}`,
+});
+
+export const createHeaderForGoogleDriveFileUpload = ({ access },
+    { title, accessToken, fileId, mimeType }) => ({
+    Authorization: `Bearer ${access}`,
+    body: JSON.stringify({
+        title,
+        accessToken,
+        fileId,
+        mimeType,
+    }),
+});
+
+
+export const createHeaderForDropboxUpload = ({ access },
+    { title, fileUrl }) => ({
+    Authorization: `Bearer ${access}`,
+    body: JSON.stringify({
+        title,
+        fileUrl,
+    }),
 });
 
