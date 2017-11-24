a32c6c5f9875ea966d44f51d041c9aac758ff0f1
pprabesh <prabes.pathak@gmail.com>
2017-11-24 17:16:18 +0545

    Add Modal for 'Add New Category' button

--- frontend/src/topic/CategoryEditor/views/CategoryView.js
+++ frontend/src/topic/CategoryEditor/views/CategoryView.js
@@ -3,9 +3,18 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {
     SelectInput,
+    TextInput,
+    TextArea,
 } from '../../../public/components/Input';
 import {
+    Modal,
+    ModalBody,
+    ModalHeader,
+    ModalFooter,
+} from '../../../public/components/View';
+import {
     PrimaryButton,
+    DangerButton,
 } from '../../../public/components/Action';
 import styles from './styles.scss';
 import Categories from '../components/Categories';
@@ -22,7 +31,24 @@ const defaultProps = {
 export default class CategoryView extends React.PureComponent {
     static propTypes = propTypes;
     static defaultProps = defaultProps;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            // Add Modal state
+            addNewCategory: false,
+
+        };
+    }
+    handleAddNewCategoryClick = () => {
+        this.setState({ addNewCategory: true });
+    }
+    handleAddNewCategoryClose = () => {
+        this.setState({ addNewCategory: false });
+    }
     render() {
+        const { addNewCategory } = this.state;
         return (
             <div styleName={this.props.className}>
                 <div styleName="header">
@@ -35,12 +61,45 @@ export default class CategoryView extends React.PureComponent {
                     </div>
                     <div styleName="pusher" />
                     <div styleName="add-category-btn">
-                        <PrimaryButton>
+                        <PrimaryButton
+                            onClick={this.handleAddNewCategoryClick}
+                        >
                             Add New Category
                         </PrimaryButton>
                     </div>
                 </div>
                 <Categories />
+                <Modal
+                    closeOnEscape
+                    onClose={this.handleAddNewCategoryClose}
+                    show={addNewCategory}
+                >
+                    <ModalHeader title="Add New Category" />
+                    <ModalBody>
+                        <TextInput
+                            placeholder="Enter Category Name"
+                            label="Name"
+                        />
+                        <TextArea
+                            placeholder="Enter Description"
+                            label="Description"
+                        />
+                        <TextInput
+                            placeholder="Enter Owner's Name"
+                            label="Owner"
+                        />
+                    </ModalBody>
+                    <ModalFooter>
+                        <DangerButton
+                            onClick={this.handleAddNewCategoryClose}
+                        >
+                            Cancel
+                        </DangerButton>
+                        <PrimaryButton>
+                            Save
+                        </PrimaryButton>
+                    </ModalFooter>
+                </Modal>
             </div>
         );
     }
