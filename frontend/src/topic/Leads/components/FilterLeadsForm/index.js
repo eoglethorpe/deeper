f07f2491234b28a7ca4041e0cbfbe7300b3bfaa2
thenav56 <ayernavin@gmail.com>
2017-11-28 14:09:43 +0545

    Add clear button in leads list page
    Add search to leads list

--- frontend/src/topic/Leads/components/FilterLeadsForm/index.js
+++ frontend/src/topic/Leads/components/FilterLeadsForm/index.js
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { Button, DangerButton } from '../../../../public/components/Action';
+import { PrimaryButton, DangerButton } from '../../../../public/components/Action';
 import {
     Form,
     SelectInput,
+    TextInput,
     DateFilter,
 } from '../../../../public/components/Input';
 
 import { RestBuilder } from '../../../../public/utils/rest';
-import { isTruthy } from '../../../../public/utils/common';
+import { isTruthy, isObjectEmpty } from '../../../../public/utils/common';
 
 import {
     createParamsForUser,
@@ -26,6 +27,7 @@ import {
     tokenSelector,
 
     setLeadFilterOptionsAction,
+    unsetLeadPageFilterAction,
 } from '../../../../common/redux';
 
 import styles from './styles.scss';
@@ -40,6 +42,7 @@ const propTypes = {
     leadFilterOptions: PropTypes.object.isRequired, // eslint-disable-line 
     value:  PropTypes.object.isRequired, // eslint-disable-line
     setLeadPageFilter: PropTypes.func.isRequired,
+    unsetLeadPageFilter: PropTypes.func.isRequired,
 };
 
 const defaultProps = {
@@ -55,6 +58,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
     setLeadFilterOptions: params => dispatch(setLeadFilterOptionsAction(params)),
     setLeadPageFilter: params => dispatch(setLeadPageFilterAction(params)),
+    unsetLeadPageFilter: params => dispatch(unsetLeadPageFilterAction(params)),
 });
 
 @connect(mapStateToProps, mapDispatchToProps)
@@ -74,6 +78,7 @@ export default class FilterLeadsForm extends React.PureComponent {
         };
 
         this.formElements = [
+            'search',
             'assignee',
             'created_at',
             'published_on',
@@ -89,7 +94,10 @@ export default class FilterLeadsForm extends React.PureComponent {
     componentWillReceiveProps(nextProps) {
         const { value, activeProject } = nextProps;
         if (this.props.value !== value) {
-            this.setState({ formValues: value });
+            this.setState({
+                formValues: value,
+                stale: false,
+            });
         }
 
         if (this.props.activeProject !== activeProject) {
@@ -172,6 +180,17 @@ export default class FilterLeadsForm extends React.PureComponent {
         });
     }
 
+    handleClearFilters = (e) => {
+        e.preventDefault();
+        if (!this.state.stale) {
+            this.props.unsetLeadPageFilter();
+        } else {
+            this.setState({
+                formValues: {},
+            });
+        }
+    }
+
     render() {
         const {
             className,
@@ -189,6 +208,8 @@ export default class FilterLeadsForm extends React.PureComponent {
             assignee,
         } = leadFilterOptions;
 
+        const isFilterEmpty = isObjectEmpty(formValues);
+
         return (
             <Form
                 styleName="filters"
@@ -197,6 +218,16 @@ export default class FilterLeadsForm extends React.PureComponent {
                 changeCallback={this.handleChange}
                 elements={this.formElements}
             >
+                <TextInput
+                    label="Search"
+                    type="search"
+                    showLabel
+                    showHintAndError={false}
+                    placeholder="Nepal"
+                    formname="search"
+                    styleName="filter text-input"
+                    value={formValues.search}
+                />
                 <SelectInput
                     label="Assigned to"
                     showLabel
@@ -235,6 +266,7 @@ export default class FilterLeadsForm extends React.PureComponent {
                     labelSelector={FilterLeadsForm.optionLabelSelector}
                     keySelector={FilterLeadsForm.optionKeySelector}
                     value={formValues.confidentiality}
+                    multiple
                 />
                 <SelectInput
                     label="Status"
@@ -246,16 +278,24 @@ export default class FilterLeadsForm extends React.PureComponent {
                     labelSelector={FilterLeadsForm.optionLabelSelector}
                     keySelector={FilterLeadsForm.optionKeySelector}
                     value={formValues.status}
+                    multiple
                 />
-                <Button
-                    styleName="apply-filter-btn"
-                    disabled={!stale}
+                <PrimaryButton
+                    styleName="filter-btn"
+                    disabled={!stale || isFilterEmpty}
                 >
                     Apply Filter
-                </Button>
+                </PrimaryButton>
+                <DangerButton
+                    styleName="filter-btn"
+                    disabled={isFilterEmpty}
+                    onClick={this.handleClearFilters}
+                >
+                    Clear Filter
+                </DangerButton>
                 {isTruthy(this.props.value.similar) && (
                     <DangerButton
-                        styleName="apply-filter-btn"
+                        styleName="filter-btn"
                         onClick={this.handleClearSimilarSelection}
                     >
                         Clear Similarity Filter
