607310685cf75dd8d1986d29f6116a682a5b0137
tnagorra <weathermist@gmail.com>
2017-12-02 16:07:29 +0545

    Minor refactor

--- frontend/src/common/reducers/domainData.js
+++ frontend/src/common/reducers/domainData.js
@@ -33,11 +33,12 @@ import initialDomainDataState from '../initial-state/domainData';
 import update from '../../public/utils/immutable-update';
 
 const setUserInformation = (state, action) => {
+    const { userId, information } = action;
     const settings = {
         users: {
-            [action.userId]: { $auto: {
+            [userId]: { $auto: {
                 information: { $auto: {
-                    $merge: action.information,
+                    $merge: information,
                 } },
             } },
         },
@@ -46,43 +47,44 @@ const setUserInformation = (state, action) => {
 };
 
 const setUsersInformation = (state, action) => {
-    const users = action.users.reduce(
-        (acc, user) => (
-            {
-                ...acc,
-                [user.id]: { $auto: {
-                    information: { $auto: {
-                        $merge: user,
-                    } },
+    const { users } = action;
+
+    const userSettings = users.reduce(
+        (acc, user) => {
+            acc[user.id] = { $auto: {
+                information: { $auto: {
+                    $merge: user,
                 } },
-            }
-        ),
+            } };
+            return acc;
+        },
         {},
     );
     const settings = {
-        users,
+        users: userSettings,
     };
     return update(state, settings);
 };
 
 const setUserProject = (state, action) => {
+    const { project, userId } = action;
     const settings = {
         projects: {
-            [action.project.id]: { $auto: {
-                $merge: action.project,
+            [project.id]: { $auto: {
+                $merge: project,
             } },
         },
     };
 
-    if (action.userId) {
-        const userProjectArrayIndex = ((state.users[action.userId] || {}).projects
-            || []).indexOf(action.project.id);
+    if (userId) {
+        const userProjectArrayIndex = ((state.users[userId] || {}).projects
+            || []).indexOf(project.id);
 
         if (userProjectArrayIndex === -1) {
             settings.users = {
-                [action.userId]: { $auto: {
+                [userId]: { $auto: {
                     projects: { $autoArray: {
-                        $push: [action.project.id],
+                        $push: [project.id],
                     } },
                 } },
             };
@@ -92,10 +94,11 @@ const setUserProject = (state, action) => {
 };
 
 const setUserProjectOptions = (state, action) => {
+    const { projectId, options } = action;
     const settings = {
         projectsOptions: {
-            [action.projectId]: { $auto: {
-                $set: action.options,
+            [projectId]: { $auto: {
+                $set: options,
             } },
         },
     };
@@ -103,21 +106,22 @@ const setUserProjectOptions = (state, action) => {
 };
 
 const unsetUserProject = (state, action) => {
+    const { projectId, userId } = action;
     const settings = {
         projects: {
-            [action.projectId]: { $auto: {
+            [projectId]: { $auto: {
                 $set: undefined,
             } },
         },
     };
 
-    if (action.userId) {
-        const userProjectArrayIndex = ((state.users[action.userId] || {}).projects
-            || []).indexOf(action.projectId);
+    if (userId) {
+        const userProjectArrayIndex = ((state.users[userId] || {}).projects
+            || []).indexOf(projectId);
 
         if (userProjectArrayIndex !== -1) {
             settings.users = {
-                [action.userId]: { $auto: {
+                [userId]: { $auto: {
                     projects: { $autoArray: {
                         $splice: [[userProjectArrayIndex, 1]],
                     } },
@@ -129,21 +133,24 @@ const unsetUserProject = (state, action) => {
 };
 
 const setUserProjects = (state, action) => {
-    const projects = action.projects.reduce((acc, project) => (
-        {
-            ...acc,
-            [project.id]: { $auto: {
+    const { projects, userId } = action;
+
+    const projectSettings = projects.reduce(
+        (acc, project) => {
+            acc[project.id] = { $auto: {
                 $set: project,
-            } },
-        }
-    ), {});
+            } };
+            return acc;
+        },
+        { },
+    );
 
     const settings = {
-        projects,
+        projects: projectSettings,
         users: {
-            [action.userId]: { $auto: {
+            [userId]: { $auto: {
                 projects: { $autoArray: {
-                    $set: action.projects.map(project => project.id),
+                    $set: projects.map(project => project.id),
                 } },
             } },
         },
@@ -152,23 +159,27 @@ const setUserProjects = (state, action) => {
 };
 
 const setUserGroups = (state, action) => {
-    const userGroups = action.userGroups.reduce(
-        (acc, userGroup) => (
-            {
-                ...acc,
-                [userGroup.id]: { $auto: {
-                    $merge: userGroup,
-                } },
-            }
-        ), {});
+    const { userGroups, userId } = action;
+
+    const userGroupSettings = userGroups.reduce(
+        (acc, userGroup) => {
+            acc[userGroup.id] = { $auto: {
+                $merge: userGroup,
+            } };
+            return acc;
+        },
+        {},
+    );
+
     const settings = {
-        userGroups,
+        userGroups: userGroupSettings,
     };
-    if (action.userId) {
+
+    if (userId) {
         settings.users = {
-            [action.userId]: { $auto: {
+            [userId]: { $auto: {
                 userGroups: { $autoArray: {
-                    $set: action.userGroups.map(userGroup => (
+                    $set: userGroups.map(userGroup => (
                         userGroup.id
                     )),
                 } },
@@ -179,23 +190,24 @@ const setUserGroups = (state, action) => {
 };
 
 const setUserGroup = (state, action) => {
+    const { userGroup, userId } = action;
     const settings = {
         userGroups: {
-            [action.userGroup.id]: { $auto: {
-                $merge: action.userGroup,
+            [userGroup.id]: { $auto: {
+                $merge: userGroup,
             } },
         },
     };
 
-    if (action.userId) {
-        const userGroupArrayIndex = ((state.users[action.userId] || {}).userGroups
-            || []).indexOf(action.userGroup.id);
+    if (userId) {
+        const userGroupArrayIndex = ((state.users[userId] || {}).userGroups
+            || []).indexOf(userGroup.id);
 
         if (userGroupArrayIndex === -1) {
             settings.users = {
-                [action.userId]: { $auto: {
+                [userId]: { $auto: {
                     userGroups: { $autoArray: {
-                        $push: [action.userGroup.id],
+                        $push: [userGroup.id],
                     } },
                 } },
             };
@@ -203,22 +215,24 @@ const setUserGroup = (state, action) => {
     }
     return update(state, settings);
 };
+
 const unsetUserGroup = (state, action) => {
+    const { userGroupId, userId } = action;
     const settings = {
         userGroups: {
-            [action.userGroupId]: { $auto: {
+            [userGroupId]: { $auto: {
                 $set: undefined,
             } },
         },
     };
 
-    if (action.userId) {
-        const userGroupArrayIndex = ((state.users[action.userId] || {}).userGroups
-            || []).indexOf(action.userGroupId);
+    if (userId) {
+        const userGroupArrayIndex = ((state.users[userId] || {}).userGroups
+            || []).indexOf(userGroupId);
 
         if (userGroupArrayIndex !== -1) {
             settings.users = {
-                [action.userId]: { $auto: {
+                [userId]: { $auto: {
                     userGroups: { $autoArray: {
                         $splice: [[userGroupArrayIndex, 1]],
                     } },
@@ -228,9 +242,12 @@ const unsetUserGroup = (state, action) => {
     }
     return update(state, settings);
 };
+
 const setUsersMembership = (state, action) => {
-    const memberships = ((state.userGroups[action.userGroupId] || {}).memberships || []);
-    const newUsersMemberShip = action.usersMembership.filter(
+    const { userGroupId, usersMembership } = action;
+
+    const memberships = ((state.userGroups[userGroupId] || {}).memberships || []);
+    const newUsersMemberShip = usersMembership.filter(
         userMembership => (
             memberships.findIndex(member => (member.id === userMembership.id)) === -1
         ),
@@ -238,7 +255,7 @@ const setUsersMembership = (state, action) => {
 
     const settings = {
         userGroups: {
-            [action.userGroupId]: { $auto: {
+            [userGroupId]: { $auto: {
                 memberships: { $autoArray: {
                     $push: newUsersMemberShip,
                 } },
@@ -247,20 +264,21 @@ const setUsersMembership = (state, action) => {
     };
     return update(state, settings);
 };
+
 const setUserMemebership = (state, action) => {
-    const memberships = ((state.userGroups[action.userGroupId] || {}).memberships || []);
+    const { userGroupId, userMembership } = action;
+
+    const memberships = ((state.userGroups[userGroupId] || {}).memberships || []);
     const updatedUsersMemberShipIndex = memberships.findIndex(
-        userMembership => (
-            action.userMembership.id === userMembership.id
-        ),
+        membership => (userMembership.id === membership.id),
     );
 
     const settings = {
         userGroups: {
-            [action.userGroupId]: { $auto: {
+            [userGroupId]: { $auto: {
                 memberships: { $autoArray: {
                     [updatedUsersMemberShipIndex]: { $auto: {
-                        $merge: action.userMembership,
+                        $merge: userMembership,
                     } },
                 } },
             } },
@@ -268,15 +286,18 @@ const setUserMemebership = (state, action) => {
     };
     return update(state, settings);
 };
+
 const unsetUserMembership = (state, action) => {
-    const memberships = ((state.userGroups[action.userGroupId] || {}).memberships || []);
+    const { userGroupId, membershipId } = action;
+
+    const memberships = ((state.userGroups[userGroupId] || {}).memberships || []);
     const groupMembershipArrayIndex = memberships.findIndex(
-        membership => (membership.id === action.membershipId));
+        membership => (membership.id === membershipId));
 
     if (groupMembershipArrayIndex !== -1) {
         const settings = {
             userGroups: {
-                [action.userGroupId]: { $auto: {
+                [userGroupId]: { $auto: {
                     memberships: { $autoArray: {
                         $splice: [[groupMembershipArrayIndex, 1]],
                     } },
@@ -287,59 +308,72 @@ const unsetUserMembership = (state, action) => {
     }
     return state;
 };
+
 const setLeadFilterOptions = (state, action) => {
+    const { projectId, leadFilterOptions } = action;
     const settings = {
         leadFilterOptions: {
-            [action.projectId]: { $autoArray: {
-                $set: action.leadFilterOptions,
+            [projectId]: { $autoArray: {
+                $set: leadFilterOptions,
             } },
         },
     };
     return update(state, settings);
 };
+
 const setRegions = (state, action) => {
-    const regions = action.regions.reduce((acc, region) => (
-        {
-            ...acc,
-            [region.id]: { $auto: {
+    const { regions } = action;
+
+    const regionSettings = regions.reduce(
+        (acc, region) => {
+            acc[region.id] = { $auto: {
                 $set: region,
-            } },
-        }
-    ), {});
+            } };
+            return acc;
+        },
+        {},
+    );
 
     const settings = {
-        regions,
+        regions: regionSettings,
     };
     return update(state, settings);
 };
+
 const setRegionDetails = (state, action) => {
+    const { regionId, regionDetails } = action;
     const settings = {
         regions: {
-            [action.regionId]: { $auto: {
-                $merge: action.regionDetails,
+            [regionId]: { $auto: {
+                $merge: regionDetails,
             } },
         },
     };
     return update(state, settings);
 };
+
 const unsetRegion = (state, action) => {
+    const { regionId } = action;
     const settings = {
         regions: {
-            [action.regionId]: { $auto: {
+            [regionId]: { $auto: {
                 $set: undefined,
             } },
         },
     };
     return update(state, settings);
 };
+
 const removeProjectRegion = (state, action) => {
+    const { projectId, regionId } = action;
+
     const settings = {};
-    const index = ((state.projects[action.projectId] || {}).regions
-        || []).findIndex(d => (d.id === action.regionId));
+    const index = ((state.projects[projectId] || {}).regions
+        || []).findIndex(d => (d.id === regionId));
 
     if (index !== -1) {
         settings.projects = {
-            [action.projectId]: { $auto: {
+            [projectId]: { $auto: {
                 regions: { $autoArray: {
                     $splice: [[index, 1]],
                 } },
@@ -348,24 +382,26 @@ const removeProjectRegion = (state, action) => {
     }
     return update(state, settings);
 };
+
 const addNewRegion = (state, action) => {
+    const { regionDetail, projectId } = action;
     const settings = {
         regions: { $auto: {
-            [action.regionDetail.id]: { $auto: {
-                $merge: action.regionDetail,
+            [regionDetail.id]: { $auto: {
+                $merge: regionDetail,
             } },
         } },
     };
-    if (action.projectId) {
-        const index = ((state.projects[action.projectId] || {}).regions
-            || []).findIndex(d => (d.id === action.regionDetail.id));
+    if (projectId) {
+        const index = ((state.projects[projectId] || {}).regions
+            || []).findIndex(d => (d.id === regionDetail.id));
         if (index === -1) {
             settings.projects = {
-                [action.projectId]: { $auto: {
+                [projectId]: { $auto: {
                     regions: { $autoArray: {
                         $push: [{
-                            id: action.regionDetail.id,
-                            title: action.regionDetail.title,
+                            id: regionDetail.id,
+                            title: regionDetail.title,
                         }],
                     } },
                 } },
@@ -374,16 +410,19 @@ const addNewRegion = (state, action) => {
     }
     return update(state, settings);
 };
+
 const setAnalysisFramework = (state, action) => {
+    const { analysisFramework } = action;
     const settings = {
         analysisFrameworks: { $auto: {
-            [action.analysisFramework.id]: { $auto: {
-                $merge: action.analysisFramework,
+            [analysisFramework.id]: { $auto: {
+                $merge: analysisFramework,
             } },
         } },
     };
     return update(state, settings);
 };
+
 const dummyAction = (state) => {
     const dummy = {
         id: 1,
@@ -404,9 +443,9 @@ const dummyAction = (state) => {
     };
     return update(state, settings);
 };
+
 const addEntry = (state, action) => {
-    const entry = action.entry;
-    const leadId = action.leadId;
+    const { entry, leadId } = action;
 
     const settings = {
         entries: { $auto: {
@@ -418,13 +457,10 @@ const addEntry = (state, action) => {
 
     return update(state, settings);
 };
-const removeEntry = (state, action) => {
-    const entryId = action.entryId;
-    const leadId = action.leadId;
 
-    const entryIndex = state.entries[leadId].findIndex(
-        d => d.id === entryId,
-    );
+const removeEntry = (state, action) => {
+    const { entryId, leadId } = action;
+    const entryIndex = state.entries[leadId].findIndex(d => d.id === entryId);
 
     const settings = {
         entries: { $auto: {
