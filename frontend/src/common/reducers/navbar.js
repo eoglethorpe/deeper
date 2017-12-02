15cbaaee28b51fd78235a7de92cd0815e2b2892a
tnagorra <weathermist@gmail.com>
2017-12-02 15:20:55 +0545

    Decrease complexity in nav reducer

--- frontend/src/common/reducers/navbar.js
+++ frontend/src/common/reducers/navbar.js
@@ -6,25 +6,36 @@ import {
 import initialNavbarState from '../initial-state/navbar';
 import update from '../../public/utils/immutable-update';
 
+const setNavbarState = (state, action) => {
+    const { visible, activeLink, validLinks } = action;
+    const settings = {
+        visible: { $set: visible },
+        activeLink: { $set: activeLink },
+        validLinks: { $set: validLinks },
+    };
+    return update(state, settings);
+};
+
+const setBlockUI = (state, action) => {
+    const { blockUI } = action;
+    const settings = {
+        blockUI: { $set: blockUI },
+    };
+    return update(state, settings);
+};
+
+const reducers = {
+    [SET_NAVBAR_STATE]: setNavbarState,
+    [SET_BLOCK_UI]: setBlockUI,
+};
+
 const navbarReducer = (state = initialNavbarState, action) => {
-    switch (action.type) {
-        case SET_NAVBAR_STATE: {
-            const settings = {
-                visible: { $set: action.visible },
-                activeLink: { $set: action.activeLink },
-                validLinks: { $set: action.validLinks },
-            };
-            return update(state, settings);
-        }
-        case SET_BLOCK_UI: {
-            const settings = {
-                blockUI: { $set: action.blockUI },
-            };
-            return update(state, settings);
-        }
-        default:
-            return state;
+    const { type } = action;
+    const reducer = reducers[type];
+    if (!reducer) {
+        return state;
     }
+    return reducer(state, action);
 };
 
 export default navbarReducer;
