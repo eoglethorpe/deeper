0a9ede1b4d7f0d402741e7b28985a5997ef68d0b
pprabesh <prabes.pathak@gmail.com>
2017-12-11 12:00:31 +0545

    Add basic texts for 404 page

--- frontend/src/topic/FourHundredFour/views/index.js
+++ frontend/src/topic/FourHundredFour/views/index.js
@@ -1,8 +1,10 @@
 import CSSModules from 'react-css-modules';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { pathNames } from '../../../common/constants';
 
 import styles from './styles.scss';
-import { pageTitles } from '../../../common/constants';
+import logo from '../../../img/deep-logo-white.png';
 
 const propTypes = {
 };
@@ -17,8 +19,22 @@ export default class FourHundredFour extends React.PureComponent {
 
     render() {
         return (
-            <div>
-                { pageTitles.fourHundredFour }
+            <div styleName="four-hundred-four">
+                <div styleName="error-404">Error - 404</div>
+                <div styleName="error-msg">Oops!! You have come too DEEP..</div>
+                <Link
+                    title="Go back to DEEP"
+                    to={pathNames.homeScreen}
+                    styleName="link"
+                >
+                Click here to go back to DEEP
+                </Link>
+                <img
+                    styleName="icon"
+                    src={logo}
+                    alt="DEEP"
+                    draggable="false"
+                />
             </div>
         );
     }
