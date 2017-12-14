f4b2bc16aa1d0d0024dbcbbace42bab9d783e768
Frozen Helium <fren.ankit@gmail.com>
2017-12-14 14:29:15 +0545

    Fix styling in List page of AnalysisFramework

--- frontend/src/topic/AnalysisFramework/widgetStore/index.js
+++ frontend/src/topic/AnalysisFramework/widgetStore/index.js
@@ -1,13 +1,8 @@
-import dummyOverviewWidget from './dummyOverviewWidget';
-import dummyListWidget from './dummyListWidget';
-
 import excerptWidget from './Excerpt';
 import matrix1dWidget from './Matrix1d';
 
 // Using list to maintain ordering
 const widgetStore = [
-    dummyOverviewWidget,
-    dummyListWidget,
     excerptWidget,
     matrix1dWidget,
 ];
