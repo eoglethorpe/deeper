adba1079ac78d9f8373854034516aeba27584e7c
pprabesh <prabes.pathak@gmail.com>
2017-12-15 16:30:22 +0545

    Make global string file Add strings for Leads

---
+++ frontend/src/common/constants/Strings/sources.js
@@ -0,0 +1,54 @@
+import string from '../Strings/lang/en/index';
+
+const sources = {
+
+    filterOwner: string[1],
+    filterDateCreated: string[2],
+    filterDatePublished: string[3],
+    filterConfidentiality: string[7],
+    filterStatus: string[8],
+    fitlerSearch: string[9],
+    filterApplyFilter: string[10],
+    filterPublisher: string[19],
+    filterClearFilter: string[11],
+    filterSourceType: string[20],
+
+    placeholderAny: string[14],
+    placeholderActions: string[16],
+    placeholderAnybody: string[17],
+    placeholderAnytime: string[18],
+
+    tableHeaderTitle: string[13],
+    tableHeaderOwner: string[1],
+    tableHeaderDateCreated: string[2],
+    tableHeaderNoOfEntries: string[15],
+
+    addButtonLabel: string[12],
+    previousButtonLabel: string[22],
+    nextButtonLabel: string[23],
+    removeButtonLabel: string[24],
+    saveButtonLabel: string[25],
+    saveAllButtonLabel: string[26],
+
+    noSourcePreviewLabel: string[27],
+    googleDriveLabel: string[38],
+    dropboxLabel: string[39],
+    localDiskLabel: string[40],
+    textLabel: string[41],
+
+    projectLabel: string[31],
+    titleLabel: string[13],
+    publisherLabel: string[5],
+    confidentialityLabel: string[7],
+    assigneeLabel: string[6],
+    datePublishedLabel: string[3],
+    urlLabel: string[32],
+    websiteLabel: string[33],
+    uploadError: string[37],
+
+    invalidUrl: string[30],
+    cannotPreviewUrl: string[36],
+
+};
+
+export default sources;
--- frontend/src/common/constants/index.js
+++ frontend/src/common/constants/index.js
@@ -1,6 +1,6 @@
 export { default as pageTitles } from './pageTitles';
 export { default as pathNames } from './pathNames';
 export { default as iconNames } from './iconNames';
-export { default as strings } from './strings';
+export { default as strings } from './Strings/lang/en';
 export { default as validLinks } from './validLinks';
 export { default as hideNavbar } from './hideNavbar';
