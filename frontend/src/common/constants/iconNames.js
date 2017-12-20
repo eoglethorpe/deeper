564cb9f159f22d34a628eb431ac704e6bb323ea0
AdityaKhatri <adityakhatri47@gmail.com>
2017-12-20 22:13:52 +0545

    Add hover features on assisted highlights

--- frontend/src/common/constants/iconNames.js
+++ frontend/src/common/constants/iconNames.js
@@ -1,5 +1,6 @@
 const iconNames = {
     add: 'ion-plus',
+    substract: 'ion-ios-minus-empty',
     applyAll: 'ion-ios-world',
     applyAllBelow: 'ion-android-arrow-dropdown-circle',
     apps: 'ion-android-apps',
