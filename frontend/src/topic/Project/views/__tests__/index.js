b86511dd7bef058d51894e2c627bb17b3483b0e3
AdityaKhatri <adityakhatri47@gmail.com>
2017-12-23 11:23:26 +0545

    Fix tests in project panel and project details

--- frontend/src/topic/Project/views/__tests__/index.js
+++ frontend/src/topic/Project/views/__tests__/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import { shallow } from 'enzyme';
-import ProjectPanel from '../index';
+import Project from '../index';
+
+jest.mock('mapbox-gl', () => undefined);
 
 const initialState = {
 };
@@ -15,7 +17,7 @@ describe('<ProjectPanel />', () => {
         <Provider
             store={store}
         >
-            <ProjectPanel />
+            <Project />
         </Provider>,
     );
 
