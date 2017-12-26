8d8a11b3fd966d0387e3bcbd9211a5ba8ca0538b
Bibek Dahal <bibekdahal.bd16@gmail.com>
2017-12-26 16:47:46 +0545

    Add react-css-modules and configure webpack

--- browser-extension/config/webpack.config.dev.js
+++ browser-extension/config/webpack.config.dev.js
@@ -157,14 +157,29 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.css$/,
+            test: /\.scss$/,
             use: [
-              require.resolve('style-loader'),
+              {
+                loader: require.resolve('style-loader'),
+                options: {
+                  sourceMap: true,
+                }
+              },
               {
                 loader: require.resolve('css-loader'),
                 options: {
                   importLoaders: 1,
+                  modules: true,
+                  localIdentName: '[local]-[hash:base64:5]',
+                  // localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+                  sourceMap: true,
                 },
+              },
+              {
+                loader: require.resolve('sass-loader'),
+                options: {
+                  sourceMap: true,
+                }
               },
               {
                 loader: require.resolve('postcss-loader'),
--- browser-extension/config/webpack.config.prod.js
+++ browser-extension/config/webpack.config.prod.js
@@ -166,7 +166,7 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\.scss$/,
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
@@ -183,8 +183,10 @@ module.exports = {
                         importLoaders: 1,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
+                        modules: true,
                       },
                     },
+                    require.resolve('sass-loader'),
                     {
                       loader: require.resolve('postcss-loader'),
                       options: {
