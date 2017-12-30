f4d5fb5cc7097fcc5769bc166e724112024f0a02
AdityaKhatri <adityakhatri47@gmail.com>
2017-12-30 11:33:38 +0545

    Connect excerpt widget in list tagging/view

--- frontend/src/topic/AnalysisFramework/widgetStore/ExcerptWidget/Tagging/ListWidget.js
+++ frontend/src/topic/AnalysisFramework/widgetStore/ExcerptWidget/Tagging/ListWidget.js
@@ -1,21 +1,64 @@
 import CSSModules from 'react-css-modules';
 import React from 'react';
-import styles from './styles.scss';
+import PropTypes from 'prop-types';
 
 import {
     TextArea,
 } from '../../../../../public/components/Input';
 
+import styles from './styles.scss';
+
+const TEXT = 'excerpt';
+
+const propTypes = {
+    id: PropTypes.number.isRequired,
+    api: PropTypes.object.isRequired,      // eslint-disable-line
+    attribute: PropTypes.object,      // eslint-disable-line
+};
+
+const defaultProps = {
+    attribute: undefined,
+};
+
 @CSSModules(styles)
 export default class ExcerptList extends React.PureComponent {
+    static propTypes = propTypes;
+    static defaultProps = defaultProps;
+
+    handleExcerptChange = (value) => {
+        const { id, api, attribute } = this.props;
+        api.getEntryModifier()
+            .setExcerpt(value)
+            .setAttribute(id, { ...attribute, excerpt: value })
+            .apply();
+    }
+
     render() {
+        const {
+            attribute,
+        } = this.props;
+
+        if (!attribute) {
+            return null;
+        }
+
         return (
-            <div styleName="excerpt-list">
-                <TextArea
-                    styleName="textarea"
-                    showLabel={false}
-                    showHintAndError={false}
-                />
+            <div styleName="excerpt-overview">
+                {
+                    attribute.type === TEXT ? (
+                        <TextArea
+                            onChange={this.handleExcerptChange}
+                            styleName="textarea"
+                            showLabel={false}
+                            showHintAndError={false}
+                            value={attribute.excerpt}
+                        />
+                    ) : (
+                        <div styleName="image">
+                            Image
+                        </div>
+                    )
+                }
             </div>
         );
     }
--- frontend/src/topic/AnalysisFramework/widgetStore/ExcerptWidget/View/ListWidget.js
+++ frontend/src/topic/AnalysisFramework/widgetStore/ExcerptWidget/View/ListWidget.js
@@ -1,16 +1,55 @@
 import CSSModules from 'react-css-modules';
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+    TextArea,
+} from '../../../../../public/components/Input';
 
 import styles from './styles.scss';
 
+const TEXT = 'excerpt';
+
+const propTypes = {
+    api: PropTypes.object.isRequired,      // eslint-disable-line
+    attribute: PropTypes.object,      // eslint-disable-line
+};
+
+const defaultProps = {
+    attribute: undefined,
+};
+
 @CSSModules(styles)
 export default class ExcerptTextList extends React.PureComponent {
+    static propTypes = propTypes;
+    static defaultProps = defaultProps;
+
     render() {
+        const {
+            attribute,
+        } = this.props;
+
+        if (!attribute) {
+            return null;
+        }
+
         return (
-            <div
-                styleName="excerpt-list"
-            >
-                Text or image excerpt
+            <div styleName="excerpt-overview">
+                {
+                    attribute.type === TEXT ? (
+                        <TextArea
+                            styleName="textarea"
+                            showLabel={false}
+                            showHintAndError={false}
+                            value={attribute.excerpt}
+                            readOnly
+                        />
+                    ) : (
+                        <div styleName="image">
+                            Image
+                        </div>
+                    )
+                }
             </div>
         );
     }
