b5d3517b9c661a4ad5fa834cd488c3cb0c0f3bfe
tnagorra <weathermist@gmail.com>
2018-02-09 11:23:50 +0545

    Change index to avoid conflict

--- frontend/src/common/initial-state/lang.js
+++ frontend/src/common/initial-state/lang.js
@@ -510,7 +510,7 @@ const initialLangState = {
                 5555: 'Discard',
                 5556: 'Invalid file(s) selected.',
                 5557: 'File Selection',
-                5558: 'Do you want to remove this widget?',
+                6000: 'Do you want to remove this widget?',
             },
             views: {
                 af: {
@@ -582,7 +582,7 @@ const initialLangState = {
                     cancelButtonTitle: 61,
                     editModalTitle: 792,
                     empty: 406,
-                    confirmDeletewWidget: 5558,
+                    confirmDeletewWidget: 6000,
                 },
                 api: {},
                 ce: {
