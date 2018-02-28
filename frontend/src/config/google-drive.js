7af3c0888fec190954f98a96ad1c994ce3bed31c
thenav56 <ayernavin@gmail.com>
2018-02-28 10:13:30 +0545

    Fix password reset 'Create' error
    * also change client id and key for google drive according to production
      and development

--- frontend/src/config/google-drive.js
+++ frontend/src/config/google-drive.js
@@ -1,5 +1,18 @@
-// TODO: Add logic for production key.
-export const googleDriveDeveloperKey = 'AIzaSyBuq0C5lhnpoUkd1Rs80QO0vO5vVRf3SeA';
-export const googleDriveClientId = '642927279233-ht6v3t7h37cc4gjh336sbin6hdlup2vi.apps.googleusercontent.com';
+const getDeveloperKey = () => {
+    if (process.env.NODE_ENV === 'development') {
+        return 'AIzaSyDINvjHwIS_HHsb3qCgFm_2GFHKqEUwucE';
+    }
+    return 'AIzaSyBuq0C5lhnpoUkd1Rs80QO0vO5vVRf3SeA';
+};
+
+const getClientId = () => {
+    if (process.env.NODE_ENV === 'development') {
+        return '642927279233-98drcidvhmudgv9dh70m7k66730n9rjr.apps.googleusercontent.com';
+    }
+    return '642927279233-ht6v3t7h37cc4gjh336sbin6hdlup2vi.apps.googleusercontent.com';
+};
+
+export const googleDriveDeveloperKey = getDeveloperKey();
+export const googleDriveClientId = getClientId();
 
 export const GOOGLE_SDK_URL = 'https://apis.google.com/js/api.js';
